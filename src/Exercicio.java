03c08c9c8fb5fb845acc2c5643a0b74a9eab924a
Adauto Bueno <adauto.bueno@gmail.com>
2022-08-19 13:14:23 -0300

    Algorítmo exercício atualizado

--- src/Exercicio.java
+++ src/Exercicio.java
@@ -1,22 +1,27 @@
+
 class Exercicio{
     public static void main(String[] args) {
         String str= "cat and*dogs-are-animals";
 
         String[] array = str.split(" |\\*|-");
-        System.out.println(array.length);
-        System.out.println(array[0]);
-        System.out.println(array[1]);
-        System.out.println(array[2]);
-        System.out.println(array[3]);
-        System.out.println(array[4]);
+
+        String fraseCompleta = "";
+
         for (int i = 0; i < array.length; i++){
             String primeiraLetra = array[i].substring(0, 1).toUpperCase();
             String demaisLetras = array[i].substring(1).toLowerCase();
             String palavraConcatenada = primeiraLetra + demaisLetras;
 
             array[i] = palavraConcatenada;
-            System.out.println(array[i]);
+
+            if ( i == 0)
+                fraseCompleta = array[i].toLowerCase();
+            else
+                fraseCompleta += array[i];
+
+
         }
+            System.out.println(fraseCompleta);
 
 
     }
