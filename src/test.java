b24a2ba8e82eb4a70e8642938901587f3b02d54c
Adauto Bueno <adauto.bueno@gmail.com>
2023-02-17 13:09:14 -0300

    Arrays.binarySearch

---
+++ src/test.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("teste");
+
+        int numeros[] = {10, 23, 56, 3, 9, 0, -20, -34, 98};
+
+        Arrays.sort(numeros); // ordena o array
+
+        int idx = Arrays.binarySearch(numeros, 10); // faz a busca binária
+
+        System.out.println(idx);
+        System.out.println(numeros[5]);
+    }
+
+}
